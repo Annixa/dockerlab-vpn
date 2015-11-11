f583eb7cb91fe3cd00d53be23518ba4749d124a6
Adam Kenyon <akenyonpro@gmail.com>
2015-11-11 13:12:59 -0800

    Adding scripts and added a bit to the README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # openvpn
+A couple scripts for installing and configuring an OpenVPN Server in a Docker container.
+
+Currently using https://hub.docker.com/r/kylemanna/openvpn/ for the OpenVPN build, which seems to work well.
---
+++ gen_client_cert.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+read -p "Enter the name of the new client: " CLIENTNAME
+
+# The easyrsa tool will prompt for the CA password. This is
+# the password we set above during the ovpn_initpki command.
+# Create the client certificate:
+docker run --volumes-from ovpn-data \
+	--rm \
+	-it \
+	kylemanna/openvpn \
+	easyrsa build-client-full $CLIENTNAME nopass
+
+# The clients need the certificates and a configuration file
+# to connect. The embedded scripts automate this task and
+# enable the user to write out a configuration to a single
+# file that can then be transfered to the client. 
+docker run --volumes-from ovpn-data \
+	--rm \
+	-it kylemanna/openvpn \
+	ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn
+
+echo "$CLIENTNAME.ovpn generated."
+echo "NOTE: Make sure to remove $CLIENTNAME.ovpn when you're done with it."
+
